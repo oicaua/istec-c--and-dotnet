328914e2991e0aa30245426cf28e6f132d8f3eea
Cauã Perrella <cauaperrella@gmail.com>
2023-10-24 22:45:25 +0100

    Repositório de Introdução a Programação - ISTEC - C#

---
+++ AULA_2/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace OlaMundo
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Saudação Simples
+            // Console.WriteLine("Olá, eu sou o Cauã! Qual é o seu nome?");
+            // Console.Write("Meu nome é: ");
+            // string nome = Console.ReadLine();
+            // Console.WriteLine("Belo nome, " + nome);
+
+            // 2. Maiusculas e Minusculas
+            // não feito
+
+            // 3. Concatenar Palavras
+            // Console.WriteLine("Escreva duas palavras e serão transformadas numa só");
+            // string palavraUm = Console.ReadLine();
+            // string palavraDois = Console.ReadLine();
+            // Console.WriteLine("As palavras viraram: " + palavraUm + palavraDois);
+
+            // 4. Soma de Dois Números 
+            // Console.WriteLine("Soma de valores");
+            // int numInt1;
+            // int numInt2;
+            // Console.Write("Número 1: ");
+            // string num1 = Console.ReadLine();
+            // Console.Write("Número 2: ");
+            // string num2 = Console.ReadLine();
+            // numInt1 = int.Parse(num1);
+            // numInt2 = int.Parse(num2);
+            // int soma = numInt1 + numInt2;
+            // Console.WriteLine("A soma é: " + soma);
+
+            // 5. Contar letras
+            // Console.WriteLine("Escreva uma palava para contar a quant de letras");
+            // string word = Console.ReadLine();
+            // var wordLength = word.Length;
+            // Console.WriteLine("A quant de letras é: " + wordLength);
+
+            // 6. Substituir Letras
+            // Console.WriteLine("Escreva uma palavra as letras a sua escolha serão subsituidas");
+            // Console.Write("A palavra (Ex.: Massa): ");
+            // string word = Console.ReadLine();
+
+            // Console.Write("Qual(is) letra(s) será(ão) substituída(s) (Ex.: 'a'): ");
+            // string letra1 = Console.ReadLine();
+
+            // Console.Write("Qual letra substituirá a(s) anterior(es) (Ex.: 'o'): ");
+            // string letra2 = Console.ReadLine();
+
+            // string wordReplaced = word.ToLower().Replace(letra1, letra2);
+            // Console.WriteLine(wordReplaced);
+
+            // 7. Maiusculas e Minusculas
+            // Console.WriteLine("Seu nome será escrito em MAIUSCULAS e minusculas");
+            // string nome = Console.ReadLine();
+            // string nomeMaiuscula = nome.ToUpper();
+            // string nomeMinuscula = nome.ToLower();
+            // Console.WriteLine(nomeMaiuscula + " e " + nomeMinuscula);
+
+        }
+            
+        }
+    }
+
+
+
+
---
+++ aula_4/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace aula_4
+{
+    class Program {
+            static void Main(string[] args) {
+                // Console.Write("Num1: ");
+                // double num1 = Convert.ToDouble(Console.ReadLine());
+                // Console.Write("Num2: ");
+                // double num2 = Convert.ToDouble(Console.ReadLine());
+                // Console.WriteLine($"A soma é {num1+num2}\nA subtração é {num1-num2}\nA multiplicação é {num1+num2}");
+
+                // Console.WriteLine("Crescimento de Seguidores no Instagram");
+                // Console.Write("Num de Seguidores no Mês passado: ");
+                // double segMesPassado = Convert.ToDouble(Console.ReadLine());
+                // Console.Write("Num de Seguidores no Atualmente: ");
+                // double segAtual = Convert.ToDouble(Console.ReadLine());
+                // double percentual = -(((segMesPassado-segAtual) / segMesPassado) * 100);
+
+                // Console.WriteLine($"O aumento de um mês ao outro foi de {percentual}%");
+
+                // Console.WriteLine("Calculadora de IMC");
+                // Console.Write("Escreva seu Peso: ");
+                // double peso = Convert.ToDouble(Console.ReadLine());
+                // Console.Write("Escreva sua Altura (em M): ");
+                // double altura = Convert.ToDouble(Console.ReadLine());
+                // double imc = Convert.ToInt32(peso / (altura*altura));
+                // Console.WriteLine(imc);
+
+                Console.WriteLine("Tempo de Viagem");
+                Console.Write("Distância em KM: ");
+                double distancia = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Velocidade Média em km/h: ");
+                double velocidadeMedia = Convert.ToDouble(Console.ReadLine());
+                double tempo = Convert.ToInt32(distancia/velocidadeMedia);
+                Console.WriteLine($"O tempo para chegar ao destino é aproximadamente de {tempo}h");
+
+            }
+    }
+}
